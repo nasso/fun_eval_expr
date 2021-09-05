fe26bd55889237779500dedcd5d4a66144a68973
nasso <nassomails@gmail.com>
2021-09-05 02:00:05 +0200

    Add CLI args handling

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,22 @@
 module Main where
 
+import Lib (eval)
+import System.Environment (getArgs, getProgName)
+import System.Exit (ExitCode (ExitFailure), exitWith)
+import System.IO (Handle, hPutStr, hPutStrLn, stderr, stdout)
+
+hUsage :: Handle -> IO ()
+hUsage h =
+  hPutStr h "Usage: "
+    >> getProgName >>= hPutStr h
+    >> hPutStrLn h " <expression>"
+
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = do
+  args <- getArgs
+  case args of
+    [expr] -> case eval expr of
+      Left err -> hPutStrLn stderr err >> exitWith (ExitFailure 84)
+      Right val -> print val
+    [] -> hUsage stdout
+    _ -> hUsage stderr >> exitWith (ExitFailure 84)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,33 @@
 module Lib
-  (
+  ( eval,
   )
 where
+
+import Parsing (Parser, parse, real)
+
+type ParseError = String
+
+data Expr
+  = Const Double
+  | Sum Expr Expr
+  | Diff Expr Expr
+  | Prod Expr Expr
+  | Quot Expr Expr
+  | Pow Expr Expr
+
+expr :: Parser Expr
+expr = Const <$> real
+
+calc :: Expr -> Double
+calc (Const x) = x
+calc (Sum x y) = calc x + calc y
+calc (Diff x y) = calc x - calc y
+calc (Prod x y) = calc x * calc y
+calc (Quot x y) = calc x / calc y
+calc (Pow x y) = calc x ** calc y
+
+eval :: String -> Either ParseError Double
+eval e = case parse expr e of
+  Just (e, []) -> Right $ calc e
+  Just (_, extra) -> Left $ "Unexpected extra input: " ++ extra
+  _ -> Left $ "Couldn't parse expression: " ++ e
